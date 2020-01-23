72568ea8839884783979ca3a3d20fafde3de85f4
GamedevGuy <57321619+GamedevGuy@users.noreply.github.com>
2020-01-23 09:06:11 +0100

    Updated MapNavigator
    Muss nur noch an den nötigen Stellen implementiert werden

--- level/level/MapNavigator.java
+++ level/level/MapNavigator.java
@@ -8,7 +8,52 @@ import settings.Settings;
 
 /**
  * Class for application wide tracking of Player-Position within map ( use at
- * your own risk :-) )
+ * your own risk :-) ) *
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * How To:
+ * 
+ * MapNavigator wird mit einem neuen Level des LevelGenerator instantiiert
+ * 
+ * LevelGenerator gen = new LevelGenerator(); MapNavigator nav =
+ * MapNavigator.getInstance(); * nav.changeLevel(gen.generateLevel(1, false));
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Mit einer Methode des MapNavigator ermitteln wir den Startraum
+ * 
+ * nav.setStartingRoom();
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Nun haben wir Zugriff auf die Daten des aktuellen Raumes und können diesen
+ * zeichnen
+ * 
+ * nav.getCurrentRoom().getRoomData()
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Sollte ein Exit-Ereignis ausgelöst werden, muss man die Position der Spieler
+ * komponente an den Navi übergeben, der dann ermittelt in welcher Richtung der
+ * Raum verlassen wird
+ * 
+ * Direction exitdirection = changecCurrentRoomByDirection(Point2D playerpos)
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Sollte die Direction dieser Methode nich null sein, wurde der Raum gewechselt
+ * und man kann sich über die Methode den neuen aktuellen Raum ausgeben lassen
+ * 
+ * nav.getCurrentRoom().getRoomData(); ...
+ * 
+ * Außerdem ist es möglich die Koordinaten der Player Componente ansttsprechend
+ * zu aktualisieren
+ * 
+ * Point2D newcoord = getEntryCoords(exitdirection);
+ * playerEntity.getComponent(PositionComponent.class). setValue(newcoord);
+ * 
+ * ----------------------------------------------------------------------------
  * 
  * @author Roman
  *
@@ -23,16 +68,14 @@ public class MapNavigator {
 	private static MapNavigator instance;
 
 	private MapNavigator() {
-		
+
 		entryCoords = new HashMap<Direction, Point2D>();
-		
+
 		// Entry points for level change
-		entryCoords.put(Direction.NORTH, 
-				new Point2D(Settings.getWindowWidth() / 2, Settings.getBlocksize() * 2));		
+		entryCoords.put(Direction.NORTH, new Point2D(Settings.getWindowWidth() / 2, Settings.getBlocksize() * 2));
 		entryCoords.put(Direction.SOUTH,
 				new Point2D(Settings.getWindowWidth() / 2, Settings.getWindowHeight() - Settings.getBlocksize() * 2));
-		entryCoords.put(Direction.WEST, 
-				new Point2D(Settings.getBlocksize() * 2, Settings.getWindowHeight() / 2));
+		entryCoords.put(Direction.WEST, new Point2D(Settings.getBlocksize() * 2, Settings.getWindowHeight() / 2));
 		entryCoords.put(Direction.EAST,
 				new Point2D(Settings.getWindowHeight() - Settings.getBlocksize() * 21, Settings.getWindowHeight() / 2));
 	}
@@ -50,27 +93,170 @@ public class MapNavigator {
 	public void changeLevel(LevelData lvl) {
 		currentLevel = lvl;
 	}
-	
+
 	/**
-	 * @param lvl
+	 * @param room
 	 */
 	public void changecCurrentRoom(UUID room) {
+
 		currentRoom = currentLevel.getLevelRoomList().get(room);
-	}	
-	
-	public LevelData getLevel() {
-		
-		return currentLevel;		
 	}
 
 	/**
-	 * player redraw on room change
+	 * player traveres onto the next room
 	 * 
-	 * @param dir
+	 * @param playerpos
+	 */
+	public Direction changecCurrentRoomByDirection(Point2D playerpos) {
+
+		Direction leavingDirection = null;
+
+		switch (playerpos) {
+		case playerIsNorth(playerpos):
+			leavingDirection = Direction.NORTH;
+			break;
+
+		case playerIsSouth(playerpos):
+			leavingDirection = Direction.SOUTH;
+			break;
+
+		case playerIsWest(playerpos):
+			leavingDirection = Direction.WEST;
+			break;
+
+		case playerIsEast(playerpos):
+			leavingDirection = Direction.EAST;
+			break;
+
+		default:
+			break;
+		}
+
+		UUID nextRoom = currentRoom.getadjacentRoom(leavingDirection);
+
+		if (nextRoom != UUID.fromString(null)) {
+			changecCurrentRoom(nextRoom);
+			return leavingDirection;
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param
+	 */
+	public LevelData getLevel() {
+
+		return currentLevel;
+	}
+
+	public RoomData getCurrentRoom() {
+		return this.currentRoom;
+	}
+
+	/**
+	 * traverses room grid and picks first non empty room as starting room
+	 * 
+	 * @param
+	 */
+	public boolean setStartingRoom() {
+		UUID[][] roomGrid = this.currentLevel.getLevelRoomGrid();
+
+		for (int i = 0; i < roomGrid.length; i++) {
+			for (int j = 0; j < roomGrid[i].length; j++) {
+				if (roomGrid[i][j] == null) {
+					// System.out.print((new UUID(0, 0)).toString().substring(0, 4) + " ");
+				} else {
+					currentRoom = currentLevel.getLevelRoomList().get(roomGrid[i][j]);
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * player redraw position on room change
+	 * 
+	 * @param dir leaving direction of previous room
 	 * @return
 	 */
 	public Point2D getEntryCoords(Direction dir) {
-		return entryCoords.get(dir);
+
+		Direction counterdirection;
+
+		switch (dir) {
+		case NORTH:
+			counterdirection = Direction.SOUTH;
+			break;
+
+		case SOUTH:
+			counterdirection = Direction.NORTH;
+			break;
+
+		case WEST:
+			counterdirection = Direction.EAST;
+			break;
+
+		case EAST:
+			counterdirection = Direction.WEST;
+			break;
+
+		default:
+			break;
+		}
+
+		return entryCoords.get(counterdirection);
+	}
+
+	/**
+	 * checks if player is in range of northern exit
+	 * 
+	 * @param playerpos
+	 * @return
+	 */
+	private boolean playerIsNorth(Point2D playerpos) {
+		return ((playerpos.getX() > (Settings.getWindowWidth() / 2) - (Settings.getBlocksize() * 2))
+				|| (playerpos.getX() < (Settings.getWindowWidth() / 2) + (Settings.getBlocksize() * 2)))
+				&& (playerpos.getY() < (Settings.getBlocksize() * 2));
+	}
+
+	/**
+	 * checks if player is in range of southern exit
+	 * 
+	 * @param playerpos
+	 * @return
+	 */
+	private boolean playerIsSouth(Point2D playerpos) {
+		return ((playerpos.getX() > (Settings.getWindowWidth() / 2) - (Settings.getBlocksize() * 2))
+				|| (playerpos.getX() < (Settings.getWindowWidth() / 2) + (Settings.getBlocksize() * 2)))
+				&& (playerpos.getY() > (Settings.getWindowHeight() - (Settings.getBlocksize() * 2)));
+	}
+
+	/**
+	 * checks if player is in range of western exit
+	 * 
+	 * @param playerpos
+	 * @return
+	 */
+	private boolean playerIsWest(Point2D playerpos) {
+		return ((playerpos.getY() > (Settings.getWindowHeight() / 2) - (Settings.getBlocksize() * 2))
+				|| (playerpos.getY() < (Settings.getWindowHeight() / 2) + (Settings.getBlocksize() * 2)))
+				&& (playerpos.getX() < (Settings.getBlocksize() * 2));
+	}
+
+	/**
+	 * checks if player is in range of eastern exit
+	 * 
+	 * @param playerpos
+	 * @return
+	 */
+	private boolean playerIsEasth(Point2D playerpos) {
+		return ((playerpos.getY() > (Settings.getWindowHeight() / 2) - (Settings.getBlocksize() * 2))
+				|| (playerpos.getY() < (Settings.getWindowHeight() / 2) + (Settings.getBlocksize() * 2)))
+				&& (playerpos.getX() > (Settings.getWindowWidth() - (Settings.getBlocksize() * 2)));
 	}
 
 }
+
